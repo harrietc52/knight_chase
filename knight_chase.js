1e668312dffc5428e78f7d1b64de0bc95741227a
Harriet Craven <harrietc52@hotmail.co.uk>
2015-10-07 16:13:22 +0100

    knows the coors where a knight can move to

--- knight_chase.js
+++ knight_chase.js
@@ -3,7 +3,27 @@ console.log("JS loaded");
 var initializeBoard = function () {
   $("#a8").html("&#9822;");
   $("#h1").html("&#9816;");
-  console.log("hello");
 };
 
 $("#start_button").click(initializeBoard);
+
+var convertToCoords = function (string) {
+  return [string.charCodeAt(0)-97, string.charCodeAt(1)-49];
+};
+
+var convertToSquareId = function (array) {
+  return String.fromCharCode(array[0]+97, array[1]+49);
+};
+
+var knightsMoves = function(string) {
+  var array = convertToCoords(string);
+  var moves = [[2,1],[1,2],[2,-1],[1,-2],[-1,-2],[-2,-1],[-2,1],[-1,2]];
+  var outArray = []
+
+  for (var i = 0; i < 8; i++) {
+    outArray.push ([array[0]+moves[i][0], array[1]+moves[i][1] ] );
+  };
+  return outArray;
+};
+
+console.log(knightsMoves("g2"));
