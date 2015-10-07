4dfc744bbc63901600473e17525bb74f597874d5
Anindya Bhattacharyya <bat020@gmail.com>
2015-10-07 16:58:19 +0100

    green squares lighting up properly

--- knight_chase.js
+++ knight_chase.js
@@ -1,8 +1,17 @@
-console.log("JS loaded");
-
 var blackToMove = function () {
   $("#status_message").text("Black to move...");
   $("#start_button").hide();
+  var blackStartsAt = "a8";
+  var greenSquares = knightMoves(blackStartsAt);
+  var square;
+  while (greenSquares.length > 0) {
+    square = greenSquares.pop();
+    // console.log(square);
+    // console.log($("#" + square));
+    $("#" + square).removeClass("white");
+    $("#" + square).removeClass("black");
+    $("#" + square).addClass("green");
+  };
 };
 
 var initializeBoard = function () {
@@ -10,8 +19,6 @@ var initializeBoard = function () {
   $("#h1").html("&#9816;");
   blackToMove();
 };
-
-$("#start_button").click(initializeBoard);
 
 var convertToCoords = function (string) {
   return [string.charCodeAt(0)-97, string.charCodeAt(1)-49];
@@ -21,15 +28,20 @@ var convertToSquareId = function (array) {
   return String.fromCharCode(array[0]+97, array[1]+49);
 };
 
-var knightsMoves = function(string) {
+var isInRange = function (array) {
+  return((array[0] >= 0) && (array[0] <= 7) && (array[1] >= 0) && (array[1] <= 7))
+};
+
+var knightMoves = function(string) {
   var array = convertToCoords(string);
   var moves = [[2,1],[1,2],[2,-1],[1,-2],[-1,-2],[-2,-1],[-2,1],[-1,2]];
-  var outArray = []
-
+  var outArray = [];
+  var candidate = [];
   for (var i = 0; i < 8; i++) {
-    outArray.push ([array[0]+moves[i][0], array[1]+moves[i][1] ] );
+    candidate = [array[0]+moves[i][0], array[1]+moves[i][1]];
+    if (isInRange(candidate)) { outArray.push(convertToSquareId(candidate)) }
   };
   return outArray;
 };
 
-console.log(knightsMoves("g2"));
+$("#start_button").click(initializeBoard);
