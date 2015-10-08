3ba78c7c636997846acd1ed1f1ea7b7bc197e4fc
Anindya Bhattacharyya <bat020@gmail.com>
2015-10-08 11:21:57 +0100

    cannot move onto dead squares

--- knight_chase.js
+++ knight_chase.js
@@ -3,12 +3,12 @@ var moveKnight = function(player) {
     $("." + greenShade(squareColour(player))).addClass(squareColour(player));
     $("." + greenShade(squareColour(player))).unbind();
     $("." + greenShade(squareColour(player))).removeClass(greenShade(squareColour(player)));
-    $("#" + isAt(player)).html("&#x2717");
+    $("#" + knightIsAt(player)).html("&#x2717");
+    addToDeadList(knightIsAt(player));
     $("#" + player + "_is_at").html($(this)[0].id)
     $("#" + player + "_square_colour").html(squareColour(player));
     if (player === "white") {
       $(this).html("&#9816;");
-      console.log("help")
       knightToMove("black");
     } else {
       $(this).html("&#9822;");
@@ -19,9 +19,10 @@ var moveKnight = function(player) {
 };
 
 var knightToMove = function (player) {
-  $("#status_message").text(player + " to move...");
+  var cappedPlayer = (player === "white") ? "White" : "Black";
+  $("#status_message").text(cappedPlayer + " to move...");
   $("#start_button").hide();
-  var greenSquares = knightMoves(isAt(player));
+  var greenSquares = knightMoves(knightIsAt(player));
   var square;
   while (greenSquares.length > 0) {
     square = greenSquares.pop();
@@ -33,8 +34,8 @@ var knightToMove = function (player) {
 };
 
 var initializeBoard = function () {
-  $("#" + isAt("black")).html("&#9822;");
-  $("#" + isAt("white")).html("&#9816;");
+  $("#" + knightIsAt("black")).html("&#9822;");
+  $("#" + knightIsAt("white")).html("&#9816;");
   knightToMove("black");
 };
 
@@ -57,98 +58,40 @@ var knightMoves = function(string) {
   var candidate = [];
   for (var i = 0; i < 8; i++) {
     candidate = [array[0]+moves[i][0], array[1]+moves[i][1]];
-    if (isInRange(candidate)) { outArray.push(convertToSquareId(candidate)) }
+    if (isInRange(candidate) && notOnDeadList(candidate))
+      { outArray.push(convertToSquareId(candidate)) }
   };
   return outArray;
 };
 
-var isAt = function(colour) {
+var knightIsAt = function (colour) {
   return $("#" + colour + "_is_at").html();
 };
 
-var squareColour = function(player) {
+var squareColour = function (player) {
   return $("#" + player + "_square_colour").html()  === "white" ? "black" : "white";
 }
 
-var greenShade = function(colour) {
+var greenShade = function (colour) {
   return colour === "white" ? "lightgreen" : "green";
 }
 
+var notOnDeadList = function (coords) {
+  var deadList = $("#dead_squares").html();
+  while (deadList.length > 0) {
+    if (deadList.slice(0,2) === convertToSquareId(coords)) {
+      return false;
+    };
+    deadList = deadList.slice(2);
+  }
+  return true;
+}
+
+var addToDeadList = function (squareString) {
+  console.log(squareString);
+  squareString = $("#dead_squares").html() + squareString;
+  console.log(squareString);
+  $("#dead_squares").html(squareString);
+}
+
 $("#start_button").click(initializeBoard);
-
-// if (player === "white") {
-//   $(this).html("&#9816;");
-//   console.log("white knight")
-// } else {
-//   $(this).html("&#9822;");
-// };
-
-// var moveBlackKnight = function() {
-//   $(this).html("&#9822;");
-//   $("." + greenShade(squareColour(colour))).addClass(otherColour(squareColour(colour)));
-//   $("." + greenShade(squareColour(colour))).unbind();
-//   $("." + greenShade(squareColour(colour))).removeClass(greenShade(squareColour(colour)));
-//   $("#" + isAt("black")).html("&#x2717");
-//   $("#black_is_at").html($(this)[0].id);
-//   $("#black_square_colour").html(otherColour(squareColour(colour)));
-//   whiteToMove();
-// };
-//
-// var moveWhiteKnight = function() {
-//   $(this).html("&#9816;");
-//   $("." + greenShade(squareColour(colour))).addClass(otherColour(squareColour(colour)));
-//   $("." + greenShade(squareColour(colour))).unbind();
-//   $("." + greenShade(squareColour(colour))).removeClass(greenShade(squareColour(colour)));
-//   $("#" + isAt("white")).html("&#x2717");
-//   $("#white_is_at").html($(this)[0].id)
-//   $("#white_square_colour").html(otherColour(squareColour(colour)));
-//   blackToMove();
-// };
-
-// var blackToMove = function () {
-//   $("#status_message").text("Black to move...");
-//   $("#start_button").hide();
-//   var greenSquares = knightMoves(isAt("black"));
-//   var square;
-//   while (greenSquares.length > 0) {
-//     square = greenSquares.pop();
-//     $("#" + square).removeClass("white");
-//     $("#" + square).removeClass("black");
-//     $("#" + square).addClass(greenShade(squareColour("black")));
-//     $("#" + square).click(moveKnight("black"));
-//   };
-// };
-//
-// var whiteToMove = function () {
-//   $("#status_message").text("White to move...");
-//   $("#start_button").hide();
-//   var greenSquares = knightMoves(isAt("white"));
-//   var square;
-//   while (greenSquares.length > 0) {
-//     square = greenSquares.pop();
-//     $("#" + square).removeClass("white");
-//     $("#" + square).removeClass("black");
-//     $("#" + square).addClass(greenShade(squareColour("white")));
-//     $("#" + square).click(moveKnight("white"));
-//   };
-// };
-
-// var blackIsAt = function() {
-//   return $("#black_is_at").html();
-// };
-
-// var blackSquareColour = function() {
-//   return $("#black_square_colour").html();
-// };
-//
-// var whiteIsAt = function() {
-//   return $("#white_is_at").html();
-// };
-
-// var whiteSquareColour = function() {
-//   return $("#white_square_colour").html();
-// };
-
-// var otherColour = function(colour) {
-//   return colour === "white" ? "black" : "white";
-// }
