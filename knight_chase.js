aa2f0db4d35925be38eec039f5c9b4f175eda29c
Anindya Bhattacharyya <bat020@gmail.com>
2015-10-09 16:01:17 +0100

    more design tweaks and refactors

--- knight_chase.js
+++ knight_chase.js
@@ -1,3 +1,5 @@
+// fundamental game logic
+
 var placeMarker = function (player) {
   return function () {
     if ($(this).html() !== "") { return };
@@ -5,8 +7,8 @@ var placeMarker = function (player) {
     $(this).html("&#x2717");
     addToDeadList($(this).attr("id"))
     $(".square").unbind();
-    if (knightMoves(knightIsAt(player)).length === 0) {
-      declareWinner("stalemate " + player);
+    if (possKnightMoves(knightIsAt(player)).length === 0) {
+      declareWinner(capped(player) + " is blocked. BLACK WINS!");
       return;
     };
     knightToMove(other(player));
@@ -15,7 +17,7 @@ var placeMarker = function (player) {
 
 var moveKnight = function (player) {
   return function () {
-    var whichGreen = greenShade(squareColour(player));
+    var whichGreen = greenShade(player);
     $("." + whichGreen).addClass(squareColour(player));
     $("." + whichGreen).unbind();
     $("." + whichGreen).removeClass(whichGreen);
@@ -30,15 +32,15 @@ var moveKnight = function (player) {
     $("#" + player + "_square_colour").html(squareColour(player));
     $(this).html(knightChar(player));
     if (knightIsAt("white") === knightIsAt("black")) {
-      declareWinner("white");
+      declareWinner("White has captured the black knight. WHITE WINS!");
       return;
     };
     if (noMovesLeft()) {
-      declareWinner("black");
+      declareWinner("Black has evaded capture. BLACK WINS!");
       return;
     };
-    if (knightMoves(knightIsAt(player)).length === 0) {
-      declareWinner("stalemate " + player);
+    if (possKnightMoves(knightIsAt(player)).length === 0) {
+      declareWinner(capped(player) + " is blocked. BLACK WINS!");
       return;
     };
     if (anyMarkersLeft()) {
@@ -58,9 +60,9 @@ var knightToMove = function (player) {
   } else {
     updateEndOfGame();
   };
-  var greenSquares = knightMoves(knightIsAt(player));
+  var greenSquares = possKnightMoves(knightIsAt(player));
   if (greenSquares.length === 0) {
-    declareWinner("stalemate " + player);
+    declareWinner(capped(player) + " is blocked. BLACK WINS!");
     return;
   };
   var square;
@@ -68,136 +70,162 @@ var knightToMove = function (player) {
     square = greenSquares.pop();
     $("#" + square).removeClass("white");
     $("#" + square).removeClass("black");
-    $("#" + square).addClass(greenShade(squareColour(player)));
+    $("#" + square).addClass(greenShade(player));
     $("#" + square).click(moveKnight(player));
   };
 };
 
-var initializeBoard = function () {
+// starting, ending, resetting the game
+
+var initialiseBoard = function () {
   $(".square").each(function () { $(this).html("") });
   $("#a8").html(knightChar("black"));
   $("#h1").html(knightChar("white"));
+  hideRules();
   $("#black_is_at").html("a8");
   $("#black_square_colour").html("white");
   $("#white_is_at").html("h1");
   $("#white_square_colour").html("white");
   $("#dead_squares").html("");
   $("#end_of_game").html("");
-  $("#start_button").hide();
-  $("#markers_left").show();
+  $("#start_button").html("RESET GAME");
+  $("#start_button").unbind();
+  $("#start_button").click(resetGame);
   knightToMove("black");
 };
 
-var declareWinner = function (winner) {
-  if (winner === "black") {
-    $("#status_message").text("Black has evaded capture. BLACK WINS!");
-  };
-  if (winner === "white"){
-    $("#status_message").text("White has captured the black knight. WHITE WINS!");
-  };
-  if (winner[0] === 's') {
-    var player = capped(winner.slice(10))
-    $("#status_message").text(player + " is blocked. BLACK WINS!");
-  };
-  $("#start_button").show();
-  $("#markers_left").hide();
+var declareWinner = function (message) {
+  $("#status_message").text(message);
+  $("#start_button").html("START GAME");
+  $("#start_button").unbind();
+  $("#start_button").click(initialiseBoard);
 };
 
-// helper functions
+var resetGame = function () {
+  location.reload();
+};
+
+// status box management
 
 var updateMarkerCount = function () {
-  $("#markers_left").text((30 - $("#dead_squares").html().length/2) + " markers left");
+  var message = $("#status_message").text() + " ";
+  var marksLeft = 30 - $("#dead_squares").html().length/2;
+  message += marksLeft.toString();
+  message += (marksLeft === 1) ? " marker left." : " markers left.";
+  $("#status_message").text(message);
 };
 
 var updateEndOfGame = function () {
-  $("#markers_left").text("White has " + Math.floor(10 - $("#end_of_game").html().length/2) + " moves left");
+  var message = $("#status_message").text() + " White has ";
+  var movesLeft = Math.floor(10 - $("#end_of_game").html().length/2);
+  message += movesLeft.toString();
+  message += (movesLeft === 1) ? " move left." : " moves left.";
+  $("#status_message").text(message);
 };
 
-var anyMarkersLeft = function () {
-  return $("#dead_squares").html().length < 60;
-};
+var showRules = function() {
+  $("#board").hide();
+  $("#rules_button").text("HIDE RULES");
+  $("#rules_button").unbind();
+  $("#rules_button").click(hideRules);
+  $("#rules").show();
+}
 
-var noMovesLeft = function () {
-  return $("#end_of_game").html().length === 19;
-};
+var hideRules = function() {
+  $("#rules").hide();
+  $("#rules_button").text("SHOW RULES");
+  $("#rules_button").unbind();
+  $("#rules_button").click(showRules);
+  $("#board").show();
+}
 
-var convertToCoords = function (string) {
-  return [string.charCodeAt(0)-97, string.charCodeAt(1)-49];
-};
+// calculating possible knight moves
 
-var convertToSquareId = function (array) {
-  return String.fromCharCode(array[0]+97, array[1]+49);
-};
-
-var isInRange = function (array) {
-  return((array[0] >= 0) && (array[0] <= 7) && (array[1] >= 0) && (array[1] <= 7))
-};
-
-var knightMoves = function (string) {
-  var array = convertToCoords(string);
+var possKnightMoves = function (string) {
+  var xy = convertToCoords(string);
   var moves = [[2,1],[1,2],[2,-1],[1,-2],[-1,-2],[-2,-1],[-2,1],[-1,2]];
   var outArray = [];
   var candidate = [];
   for (var i = 0; i < 8; i++) {
-    candidate = [array[0]+moves[i][0], array[1]+moves[i][1]];
+    candidate = [xy[0]+moves[i][0], xy[1]+moves[i][1]];
     if (isInRange(candidate) && notOnDeadList(candidate))
       { outArray.push(convertToSquareId(candidate)) }
   };
   return outArray;
 };
 
+var convertToCoords = function (string) {
+  return [string.charCodeAt(0)-97, string.charCodeAt(1)-49];
+};
+
+var convertToSquareId = function (xy) {
+  return String.fromCharCode(xy[0]+97, xy[1]+49);
+};
+
+var isInRange = function (xy) {
+  return((xy[0] >= 0) && (xy[0] <= 7) && (xy[1] >= 0) && (xy[1] <= 7))
+};
+
 var lastPossibleMove = function (player) {
-  var array = knightMoves(knightIsAt(other(player)));
-  if (array.length === 1) {
-    return array[0];
-  }
-  return null;
+  var array = possKnightMoves(knightIsAt(other(player)));
+  return (array.length === 1) ? array[0] : null;
 }
 
-var knightIsAt = function (colour) {
-  return $("#" + colour + "_is_at").html();
+var notOnDeadList = function (xy) {
+  var deadList = $("#dead_squares").html();
+  while (deadList.length > 0) {
+    if (deadList.slice(0,2) === convertToSquareId(xy)) { return false }
+    deadList = deadList.slice(2);
+  }
+  return true;
+};
+
+// miscellaneous helper functions
+
+var addToDeadList = function (squareString) {
+  $("#dead_squares").append(squareString);
+};
+
+var anyMarkersLeft = function () {
+  return $("#dead_squares").html().length < 60;
+};
+
+var addToEndOfGame = function () {
+  $("#end_of_game").append("X");
+};
+
+var noMovesLeft = function () {
+  return $("#end_of_game").html().length === 19;
+};
+
+var knightIsAt = function (player) {
+  return $("#" + player + "_is_at").html();
+};
+
+var greenShade = function (player) {
+  return (squareColour(player) === "white") ? "lightgreen" : "green";
 };
 
 var squareColour = function (player) {
   return other($("#" + player + "_square_colour").html());
-}
-
-var greenShade = function (colour) {
-  return colour === "white" ? "lightgreen" : "green";
-}
-
-var notOnDeadList = function (coords) {
-  var deadList = $("#dead_squares").html();
-  while (deadList.length > 0) {
-    if (deadList.slice(0,2) === convertToSquareId(coords)) {
-      return false;
-    };
-    deadList = deadList.slice(2);
-  }
-  return true;
-}
-
-var addToDeadList = function (squareString) {
-  squareString = $("#dead_squares").html() + squareString;
-  $("#dead_squares").html(squareString);
-}
-
-var addToEndOfGame = function () {
-  $("#end_of_game").html($("#end_of_game").html() + "X");
-}
+};
 
 var knightChar = function (player) {
   return (player === "white") ? "&#9816;" : "&#9822;";
-}
+};
 
 var capped = function (player) {
   return (player === "white") ? "White" : "Black";
-}
+};
 
-var other = function (player) {
-  return (player === "white") ? "black" : "white";
-}
+var other = function (colour) {
+  return (colour === "white") ? "black" : "white";
+};
+
+// initialise game controller
 
 $(document).ready(function () {
-  $("#start_button").click(initializeBoard);
+  $("#start_button").click(initialiseBoard);
+  $("#rules_button").click(showRules);
+  $("#done_button").click(hideRules);
 });
